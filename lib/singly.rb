c5c104b906c4afbe1a87660bff770881d115d6aa
Chris Reister <chris@chrisreister.com>
2012-12-02 09:36:40 -0800

    make the login button work directly, and work from production or dev.

--- lib/singly.rb
+++ lib/singly.rb
@@ -6,6 +6,15 @@ module Singly
   CLIENT_ID = "28d73ba2e4eac68eff50c8856d9e2c8f".freeze
   CLIENT_KEY = "58bdf3fb1b31eb93886b476da2535e3b".freeze
   
+  def self.auth_callback
+    val = if( Rails.env == "production" )
+      "http://webgoggl.es/auth"
+    else
+      "http://localhost:3000/auth"
+    end
+    return URI.escape( val, "/:" )
+  end
+  
 end
 
 require './lib/singly/facebook'
