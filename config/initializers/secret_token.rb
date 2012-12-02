c7082e56bfbeccf1f0348fe5634fc8f67e449d73
Chris Reister <chris@chrisreister.com>
2012-12-01 17:46:37 -0800

    files for rails and bootstrap and backbone

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webgoggles::Application.config.secret_token = 'a8a2218f1e1ea47103c763b7f9aeb0e918755088667ea2a1f519c447a55115d9408b92c161fd8250f4e0e0cb01aa3d2980472e2efd90ebdd7af452ddaae30084'
