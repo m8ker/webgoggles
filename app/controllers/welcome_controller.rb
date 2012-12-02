dc9043557722c7e50763eaf985c0f61937b6fea9
Chris Reister <chris@chrisreister.com>
2012-12-02 00:08:13 -0800

    loads 20 friends on index if logged in.  shows the names, and wrong pictures.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,25 +2,30 @@ require 'httparty'
 
 class WelcomeController < ApplicationController
   def index
+    @friends = []
+    @logged_in = true
+    if session['access_token']
+      @friends = HTTParty.get("#{SINGLY_API_BASE}/friends/facebook", :query => {:sort => "photos" , :access_token => session['access_token']}).parsed_response
+    else
+      @logged_in = false
+    end
   end
   
   def login
   end
   
   def auth
-    puts params.inspect
     data = HTTParty.post("https://api.singly.com/oauth/access_token", :body => {
       :client_id => SINGLY_CLIENT_ID,
       :client_secret => SINGLY_CLIENT_KEY,
       :code => params[:code]})
-    puts data.inspect
     data = data.parsed_response
-    puts data.inspect
     session[:account] = data["account"]
     session[:access_token] = data["access_token"]
-    puts session.inspect
     redirect_to "/"
   end
+  
+  SINGLY_API_BASE = "https://api.singly.com"
   
   SINGLY_CLIENT_ID = "28d73ba2e4eac68eff50c8856d9e2c8f".freeze
   SINGLY_CLIENT_KEY = "58bdf3fb1b31eb93886b476da2535e3b".freeze
